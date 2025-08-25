9556270b93f8e83069f7413226857e529672d141
naveen-s-chand_atko <naveen.s.chand@okta.com>
2025-08-25 23:31:00 +0530

    fix(examples): fix props

--- examples/react-spa-npm/src/views/user-profile-page.tsx
+++ examples/react-spa-npm/src/views/user-profile-page.tsx
@@ -5,27 +5,34 @@ const UserProfilePage = () => {
     <div className="space-y-6">
       <UserMFAMgmt
         // styling={{
-        //   common: {
-        //     '--font-size-heading': '50px',
-        //     '--font-size-description': '1.25rem',
-        //     '--font-size-title': '30px',
-        //     '--font-size-paragraph': '0.875rem',
-        //     '--font-size-label': '0.875rem',
+        //   variables: {
+        //     common: {
+        //       '--font-size-heading': '50px',
+        //       '--font-size-description': '1.25rem',
+        //       '--font-size-title': '30px',
+        //       '--font-size-paragraph': '0.875rem',
+        //       '--font-size-label': '0.875rem',
+        //     },
+        //     light: {
+        //       '--color-primary': 'blue',
+        //       '--color-primary-foreground': 'white',
+        //       '--color-foreground': 'orange',
+        //       '--color-muted-foreground': 'orange',
+        //       '--color-accent-foreground': '#a1a1ee',
+        //     },
+        //     dark: {
+        //       '--color-primary': 'red',
+        //       '--color-primary-foreground': 'white',
+        //       '--color-foreground': 'red',
+        //       '--color-muted-foreground': 'green',
+        //       '--color-accent-foreground': '#a1a1ee',
+        //     },
         //   },
-        //   light: {
-        //     '--color-primary': 'blue',
-        //     '--color-primary-foreground': 'white',
-        //     '--color-foreground': 'orange',
-        //     '--color-muted-foreground': 'orange',
-        //     '--color-accent-foreground': '#a1a1ee',
-        //   },
-        //   dark: {
-        //     '--color-primary': 'red',
-        //     '--color-primary-foreground': 'white',
-        //     '--color-foreground': 'red',
-        //     '--color-muted-foreground': 'green',
-        //     '--color-accent-foreground': '#a1a1ee',
-        //   },
+        //   classes: {
+        //     'DeleteFactorConfirmation-dialogContent': '',
+        //     'UserMFAMgmt-card': '',
+        //     'UserMFASetupForm-dialogContent': ''
+        //   }
         // }}
         factorConfig={{
           duo: {
